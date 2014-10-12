ea7bff48c71d9e57fc99a10c4da67cd3df91af15
Steven Swenson <ctigeek@gmail.com>
2014-10-11 21:32:27 -0400

    First commit!

---
+++ MySqlCmdlet.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Data;
+using System.Management.Automation;
+using MySql.Data.MySqlClient;
+
+namespace InvokeMySqlCmd
+{
+    [Cmdlet("Invoke", "MySqlcmd")]
+    public class MySqlCmdlet : Cmdlet
+    {
+        public MySqlCmdlet()
+        {
+            Server = "localhost";
+            QueryTimeout = 30;
+        }
+
+        [Parameter(Mandatory = true)]
+        public string Query { get; set; }
+
+        [Parameter(Mandatory = true)]
+        public string Username { get; set; }
+
+        [Parameter(Mandatory = true)]
+        public string Password { get; set; }
+
+        [Parameter(Mandatory = false)]
+        public int QueryTimeout { get; set; }
+
+        [Parameter(Mandatory = false)]
+        public string InputFile { get; set; }
+
+        [Parameter(Mandatory = false)]
+        public SwitchParameter Scalar { get; set; }
+
+        [Parameter(Mandatory = false)]
+        public SwitchParameter NonQuery { get; set; }
+
+        [Parameter(Mandatory = false)]
+        public string Database { get; set; }
+
+        [Parameter(Mandatory = false)]
+        public string Server { get; set; }
+
+        protected override void BeginProcessing()
+        {
+            if (!string.IsNullOrEmpty(Query) && !string.IsNullOrEmpty(InputFile))
+            {
+                throw new ArgumentException("You cannot specify both Query and InputFile.");
+            }
+            if (string.IsNullOrEmpty(Query) && string.IsNullOrEmpty(InputFile))
+            {
+                throw new ArgumentException("You must specify a Query or an InputFile containing a valid query to run.");
+            }
+            WriteDebug("Input validated.");
+            base.BeginProcessing();
+        }
+
+        protected override void ProcessRecord()
+        {
+            using (var connection = new MySqlConnection(CreateConnectionString()))
+            {
+                connection.Open();
+                WriteDebug("Connection opened.");
+                var dataTable = new DataTable();
+                var command = GetCommand(connection);
+
+                if (Scalar)
+                {
+                    WriteDebug("Running scalar query...");
+                    var result = command.ExecuteScalar();
+                    WriteDebug("Query complete. Retrieved scalar result:" + result.ToString() );
+                    WriteObject(result);
+                }
+                else if (NonQuery)
+                {
+                    WriteDebug("Running NonQuery query...");
+                    var result = command.ExecuteNonQuery();
+                    WriteDebug("NonQuery query complete. " + result + " rows affected.");
+                    WriteObject(result);
+                }
+                else
+                {
+                    WriteDebug("Running query....");
+                    using (var adapter = new MySqlDataAdapter(command))
+                    {
+                        adapter.Fill(dataTable);
+                        WriteDebug("Query complete. Returned " + dataTable.Rows.Count + " rows.");
+                        var resultSet = GetDataRowArrayFromTable(dataTable);
+                        WriteObject(resultSet);
+                    }
+                }
+            }
+            base.ProcessRecord();
+        }
+
+        private DataRow[] GetDataRowArrayFromTable(DataTable dataTable)
+        {
+            var resultSet = new DataRow[dataTable.Rows.Count];
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                resultSet[i] = dataTable.Rows[i];
+            }
+            return resultSet;
+        }
+
+        private string CreateConnectionString()
+        {
+            var formatString = "Data Source={0};Initial Catalog={1};User ID={2};Password={3}";
+            var connectionString = string.Format(formatString, Server, Database, Username, Password);
+            WriteDebug("Using the following connection string: " + connectionString);
+            return connectionString;
+        }
+
+        private MySqlCommand GetCommand(MySqlConnection connection)
+        {
+            var sql = Query;
+            if (!string.IsNullOrEmpty(InputFile))
+            {
+                WriteDebug("Using query in file " + InputFile);
+                sql = new System.IO.StreamReader(InputFile).ReadToEnd();
+            }
+            var command = new MySqlCommand(sql, connection);
+            command.CommandTimeout = QueryTimeout;
+
+            return command;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,31 @@ InvokeMySqlCmd
 ==============
 
 Powershell Cmdlet for MySql with similar functionality to the Sql Server cmdlet Invoke-SqlCmd.
+
+## How to install
+1. Download the dlls from the latest release, or build it yourself from source.
+2. Copy the dlls to one of the defined paths in the PSModulePath environment variable "$env:PSModulePath".
+	See also http://msdn.microsoft.com/en-us/library/dd878350(v=vs.85).aspx
+3. Import the module by running "Import-Module InvokeMySqlcmd" in your powershell script. I'm sure there's a way to register the module permanently but I haven't found it.
+
+## How to use
+1. Make sure you call "Import-Module InvokeMySqlCmd" in each script that uses it.
+2. Call the cmdlet with the correct parameters:
+	-Query  The query to run. This is required unless InputFile is included. If you are piping anything into this cmdlet, it will go into the Query parameter.
+	-Username (required) The MySql username to use.
+	-Password (required) The MySql password for the user.
+	-QueryTimeout The timeout for the sql command. Default is 30 seconds.
+	-InputFile The path to a file containing the sql command to run. If you specify this, do not include the Query parameter.
+	-Scalar (switch) If included, the result will be the value in the first column of the first row returned.
+	-NonQuery (switch) Include this if you don't expect rows to be returned (insert/update/delete). The value returned is the number of rows affected.
+	-Database The default schema. This translates to "initial catalog" in the connection string built by the cmdlet.
+	-Server The name of the server to connect to. This translates to "Data Source" in the connection string. Default is "localhost".
+
+ex:
+Invoke-MySqlcmd 
+	
+## What's returned
+	* For normal select queries, an array of System.Data.DataRow. If the query doesn't return anything, that will be an array with 0 elements.
+	* If you include the Scalar switch, it will be the datatype of the first column of the first row returned.
+	* If you include the NonQuery switch, it will return the number of rows affected by the query.
+	
